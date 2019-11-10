ef9940eb8dcf654ff04116cc36e6214c76fb0dc4
Alexander <divinenickname@gmail.com>
2019-11-10 15:58:55 +0500

    HW 01

---
+++ src/main/java/servlets/MirrorServlet.java
@@ -0,0 +1,21 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MirrorServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if(req.getParameter("key")!=null){
+            resp.setStatus(HttpServletResponse.SC_OK);
+            resp.getWriter().println(req.getParameter("key"));
+        }
+        else {
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        }
+
+    }
+}
